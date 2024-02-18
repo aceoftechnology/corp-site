e7cf573940a573e7fcbf155c930f3b821b5941dc
suguru-toyohara <suguru_toyohara@aceof.technology>
2024-02-18 18:00:49 +0900

    fmt

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -1,23 +1,22 @@
 import { Box } from '@mui/material';
 import Navigators from './Navigators';
 import CorpName from './CorpName';
-import Divider from '../Divider';
+import Divider from '@/components/layout/Divider';
 
 export default function Header() {
   return (
-    // TODO: これで良いかあとで確認
     // TODO: TopPageと別のページでの表示を変える
     // TODO: stickyにする
-      <header>
-        <Box className="flex text-slate-100 px-2 pt-2 my-4 mx-24">
-          <Box className="flex-none">
-            <CorpName />
-          </Box>
-          <Box className="grow">
-            <Navigators />
-          </Box>
+    <header>
+      <Box className="flex text-slate-100 px-2 pt-2 my-4 mx-24">
+        <Box className="flex-none">
+          <CorpName />
         </Box>
-        <Divider />
-      </header>
+        <Box className="grow">
+          <Navigators />
+        </Box>
+      </Box>
+      <Divider />
+    </header>
   );
 }
