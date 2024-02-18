8574118ca1eb76a6f92ed93468be62d467dfb66e
suguru-toyohara <suguru_toyohara@aceof.technology>
2024-02-18 16:33:09 +0900

    add todo & apply header/footer to layout.tsx

--- src/components/layout/footer/index.tsx
+++ src/components/layout/footer/index.tsx
@@ -5,6 +5,7 @@ export default function Footer() {
   return (
     <footer>
       <Divider />
+      {/* TODO: 色味変えたほうが良いかも。 */}
       <Box className="bg-gray-50 text-slate-900 px-2 pb-2 w-full h-32 flex items-center justify-center">
         <p className="text-gray-500">&copy; 2024 Ace of Technology</p>
       </Box>
--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -6,6 +6,8 @@ export default function Header() {
   return (
     // TODO: これで良いかあとで確認
     // TODO: headerdで囲んで、headerにはstyleを当てない
+    // TODO: 余白を調整
+    // TODO: 色味を変える
     <Box className="bg-gray-50 text-slate-900 px-2 pt-2">
       <header className="flex">
         <Box className="flex-none">
